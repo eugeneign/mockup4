e234614fc45bf802387bc8bdd0767985ec9e2672
eugeneign <49946057+eugeneign@users.noreply.github.com>
2020-01-16 17:39:27 +0300

    16.01

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+
+	$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){  
+        $('.navbar').addClass("sticky");
+    }
+    else{
+        $('.navbar').removeClass("sticky");
+    }
+});
